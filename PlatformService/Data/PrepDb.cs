2a6c5187762cf5db9b683be9da896d23b2322f9f
José Rafael Becerra <jose.rabelo95@gmail.com>
2024-10-03 14:50:39 -0600

    Added k8s config for pvc, sql, new models and data updates to commandService

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -2,28 +2,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using PlatformService.Models;
 
 namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app, bool IsProd)
         {
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>()!);
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>()!, IsProd);
             }
         }
 
-        private static void SeedData(AppDbContext context)
+        private static void SeedData(AppDbContext context, bool IsProd)
         {
+
+            if(IsProd)
+            {
+                Console.WriteLine("INFO --> Trying to apply migrations...");
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch(Exception ex)
+                { 
+                    Console.WriteLine($"ERR --> Coudn't apply migrations, {ex.Message}");
+                }
+            }
+
             if(context.Platforms.Any())
             {
-                Console.WriteLine("Data already exists");
+                Console.WriteLine("INFO --> Data already exists");
                 return;
             }
-            Console.WriteLine("Seeding some data HOLIS QUE ROLLIs");
+            Console.WriteLine("INFO --> Seeding some data HOLIS QUE ROLLIs");
 
             context.Platforms.AddRange(
                 new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -5,13 +5,22 @@ using PlatformService.SyncDataServices.Http;
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
-// Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+//Add services to the container.
+if(builder.Environment.IsProduction())
+{ 
+    Console.WriteLine("--> Using SQL DB...");
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(config.GetConnectionString("PSConn")));
+}
+else 
+{
+    Console.WriteLine("--> Using in mem  DB...");
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+}
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-Console.WriteLine($"CommandService Endpoint --> {config["CommandSrv"]}");
+Console.WriteLine($"INFO --> CommandService Endpoint --> {config["CommandSrv"]}");
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -32,7 +41,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-PrepDb.PrepPopulation(app);
+PrepDb.PrepPopulation(app, builder.Environment.IsProduction());
 
 app.MapControllers();
 
